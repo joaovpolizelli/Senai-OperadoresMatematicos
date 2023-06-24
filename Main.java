9dfcdd3dde782fbdd61710124ddaad1ad153af41
João Vitor Polizelli <73860859+joaovpolizelli@users.noreply.github.com>
2023-06-24 16:30:05 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp.senai.br.operacoesmatematicas;
+
+/**
+ *
+ * @author aluno
+ */
+public class Main {
+    
+    public static void main(String arg[]){
+    
+        Operacoes objeto = new Operacoes();
+        
+        double soma = objeto.adicao(10, 20);
+        
+        System.out.print(soma);
+        
+    System.exit(0);
+    }
+    
+}
---
+++ Operacoes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp.senai.br.operacoesmatematicas;
+
+/**
+ *
+ * @author aluno
+ */
+public class Operacoes {
+    
+    public static double adicao(double valor1, double valor2){
+    return valor1 + valor2;
+    }
+    
+}
